17c1e2562120e6cb61bf6985b91bc39c66ea751f
Marcos Figueiredo de Castro <marcosfigueiredodecastro@gmail.com>
2025-07-07 18:13:07 -0300

    Teste3

--- src/test/java/e2e/UITest.java
+++ src/test/java/e2e/UITest.java
@@ -3,12 +3,19 @@ package e2e;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class UITest {
 
     @Test
     public void testGoogleHomepage() {
-        WebDriver driver = new ChromeDriver(); // ChromeDriver deve estar no PATH
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+
+        WebDriver driver = new ChromeDriver(options);
         driver.get("https://www.google.com");
         assert driver.getTitle().contains("Google");
         driver.quit();
